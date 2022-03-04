c9a6b41a9532d1f3b07365421bcadb75f97e4523
nikes23 <nikes23@hotmail.de>
2022-03-04 15:46:33 +0100

    Version 1.0.0

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const {check, validationResult} = require('express-validator');
 const handlebars = require('express-handlebars');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const http = require('http');
+const hostname = 'localhost';
 const port = process.env.PORT || 3000;
+//const porttwo = process.env.PORT || 3001;
 
 //Creates our express server
 const app = express();
@@ -45,118 +48,17 @@ app.get('/impressum', (req, res) => {
     res.render('main', {layout: 'impressum'});
 });
 
-app.post('/send', urlencodedParser,[
-    check('firstname', 'Der Vorname muss mindestens 3 Buchstaben lang sein')
-        .exists()
-        .isLength({min:3}),
-    check('lastname', 'Der Nachname muss mindestens 3 Buchstaben lang sein')
-        .exists()
-        .isLength({min:3}),
-    check('mail', 'Die E-Mail Adresse ist ungültig')
-        .isEmail()
-        .normalizeEmail(),
-],(req, res) => {
 
-    console.log(req.body)
-
-    const booleanSell = !!req.body.datenschutzCheckBox;
-    console.log(booleanSell)
-
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-    }
-
-    const output = `
-    <p>Sie haben eine neue Kontaktaufnahme erhalten.</p>
-    <h3>Kontaktdaten</h3>
-    <ul>
-    <li>Vorname: ${req.body.firstname}</li>
-    <li>Nachname: ${req.body.lastname}</li>
-    <li>E-Mail: ${req.body.mail}</li>
-    <li>Betreff: ${req.body.subject}</li>
-    <li>Datenschutzerklärung: Akzeptiert</li>
-    </ul>
-    <h3>Nachricht:</h3>
-    <p>${req.body.message}</p>
-    `;
-
-    //console.log(req.body);
-    /*async function main() {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'anton.kempf@hotmail.com', // generated ethereal user
-            pass: '34>*7R6g77V?6@', // generated ethereal password
-        },
-        tls:{
-            rejectUnauthorized:false
-        }
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Nodemailer Contact" <anton.kempf@hotmail.com>', // sender address
-        to: "nikes23@hotmail.de, anton.kempf@region10.group", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: output, // html body
-    });
-
-    transporter.verify(function(error, success) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Server is ready to take our messages');
-            }
-        });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        res.render('contact', {msg:'Email has been sent'})
-    }*/
-
-
-    const transport = nodemailer.createTransport({
-        host: "smtp.strato.de",
-        port: 465,
-        auth: {
-            user: "webmaster@fiberconstruct.de",
-            pass: "cnu@GBW2ahc_wrw*uhb",
-        }
-    });
-
-    const mailOptions = {
-        from: '"Fiber Construct Team" <webmaster@fiberconstruct.de>',
-        //from: req.body.mail,
-        to: 'info@fiberconstruct.de',
-        subject: 'Neue Nachricht vom Kontaktformular',
-        text: '',
-        html: output,
-        //html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br /><img src="cid:uniq-mailtrap.png" alt="mailtrap" />',
-        /*attachments: [
-            {
-                filename: 'mailtrap.png',
-                path: __dirname + '/mailtrap.png',
-                cid: 'uniq-mailtrap.png'
-            }
-        ]*/
-    };
-
-    transport.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Nachricht gesendet: %s', info.messageId);
-        res.render('main', {layout: 'index', msg:'E-Mail wurde gesendet'});
-        res.send('success')
-    });
-});
 
 app.listen(port, () => console.log('Server started on port: ' + port));
+
+
+/*const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+server.listen(porttwo, hostname, () => {
+    console.log(`Server running at http://${hostname}:${porttwo}/`);
+});*/
